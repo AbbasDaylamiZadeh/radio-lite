970e2e6b7392daa5beeebad7fa7df68e3a864f79
Abbas Daylami Zadeh <mrabbas44@gmail.com>
2025-04-04 12:42:43 +0330

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,12 @@
 import AudioPlayer from "@/components/audio-player/audio-player";
-import { MusicProvider } from "@/context/music-context";
+import {MusicProvider} from "@/context/music-context";
 
 export default function Home() {
-  return (
-    <div className="w-full h-dvh flex justify-center items-center bg-gradient-to-b  from-neutral-900 to-zinc-800">
-       <MusicProvider>
-      <AudioPlayer/>
-      </MusicProvider>
-    </div>
-  );
+    return (
+        <div className="w-full h-dvh flex justify-center items-center bg-gradient-to-b  from-neutral-900 to-zinc-800">
+            <MusicProvider>
+                <AudioPlayer/>
+            </MusicProvider>
+        </div>
+    );
 }
