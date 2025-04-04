3da997041b8bd86f48de51dbcfdc7a19677ce1df
Abbas Daylami Zadeh <mrabbas44@gmail.com>
2025-04-04 22:01:39 +0330

    Update like_dislike_music_api.ts

--- services/like_dislike_music_api.ts
+++ services/like_dislike_music_api.ts
@@ -1,8 +1,8 @@
 import {MusicResponse} from "@/_components/audio-player/types";
 
-export const likedMusicApi = async (id: number): Promise<MusicResponse> => {
+export const likedDisLikeMusicApi = async (id: number,likeOrDislike:string): Promise<MusicResponse> => {
     try {
-        const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_API}/like/${id}`, {cache: 'no-store'},);
+        const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_API}/${likeOrDislike}/${id}`, {cache: 'no-store'},);
 
         if (!res.ok) {
             throw new Error(`Failed to fetch music: ${res.statusText}`);
