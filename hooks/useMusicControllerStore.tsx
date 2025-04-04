c1b344cff545b2dddc7f7216b9c8a3f59c200751
Abbas Daylami Zadeh <mrabbas44@gmail.com>
2025-04-04 20:08:01 +0330

    Squashed commit of the following:

--- hooks/useMusicControllerStore.tsx
+++ hooks/useMusicControllerStore.tsx
@@ -1,65 +1,67 @@
-import { useMusic } from "@/context/music-context";
-import { fetchNowPlayingMusic } from "@/services/get_music_api";
-import { useCallback, useState } from "react";
+import {useMusic} from "@/context/music-context";
+import {fetchNowPlayingMusic} from "@/services/get_music_api";
+import {useCallback, useState} from "react";
 import useMusicListStore from "./useMusicListStore";
 
 export const useMusicController = () => {
 
-  const { musicData, audioRef, setMusicData, sourceRef } = useMusic();
-  const { update_music_ids, get_music_ids, reset_music_ids } = useMusicListStore();
+    const {musicData, audioRef, setMusicData, sourceRef} = useMusic();
+    const {update_music_ids, get_music_ids, reset_music_ids} = useMusicListStore();
 
-  const[isPlaying,setIsPlaying]=useState(false)
-  // Derived state instead of useState
+    const [isPlaying, setIsPlaying] = useState(false)
 
-  const handlePlayPause = useCallback(() => {
-    if (!audioRef.current) return;
-    
-    if (audioRef.current.paused) {
-      setIsPlaying(true)
-      audioRef.current.play().catch((err) => console.error("Playback error:", err));
-    } else {
-      setIsPlaying(false)
-      audioRef.current.pause();
+    const handlePlayPause = useCallback(() => {
+        if (!audioRef.current) return;
 
-    }
-  }, []);
-
-  const handleNextMusic = useCallback(async () => {
-    if (!audioRef.current) return;
-    
-    audioRef.current.pause();
-    
-    const playedIds = get_music_ids();
-    try {
-      let response = await fetchNowPlayingMusic({ played_ids: playedIds });
-
-      if (response.GetMusicResponseStatusMessage === "NotFound Music") {
-        reset_music_ids();
-        response = await fetchNowPlayingMusic({ played_ids: [] });
-      }
-
-      if (response.data) {
-        update_music_ids(response.data.id);
-        setMusicData(response.data);
-        setIsPlaying(true)
-
-        if (sourceRef.current) {
-          sourceRef.current.src = response.data.url;
+        if (audioRef.current.paused) {
+            audioRef.current.play().catch((err) => console.error("Playback error:", err));
+            setIsPlaying(true)
+            return
+        } else {
+            audioRef.current.pause();
+            setIsPlaying(false)
+            return
         }
 
-        audioRef.current.load();
-        audioRef.current.play().catch((err) => console.error("Playback error:", err));
-      }
-    } catch (error) {
-      console.error("Error fetching music:", error);
-    }
-  }, [get_music_ids, reset_music_ids, update_music_ids, setMusicData]);
 
-  return {
-    isPlaying,
-    musicData,
-    audioRef,
-    handlePlayPause,
-    handleNextMusic,
-  };
+    }, []);
+
+    const handleNextMusic = useCallback(async () => {
+        if (!audioRef.current) return;
+
+        audioRef.current.pause();
+
+        const playedIds = get_music_ids();
+        try {
+            let response = await fetchNowPlayingMusic({played_ids: playedIds});
+
+            if (response.GetMusicResponseStatusMessage === "NotFound Music") {
+                reset_music_ids();
+                response = await fetchNowPlayingMusic({played_ids: []});
+            }
+
+            if (response.data) {
+                update_music_ids(response.data.id);
+                setMusicData(response.data);
+                setIsPlaying(true)
+
+                if (sourceRef.current) {
+                    sourceRef.current.src = response.data.url;
+                }
+
+                audioRef.current.load();
+                audioRef.current.play().catch((err) => console.error("Playback error:", err));
+            }
+        } catch (error) {
+            console.error("Error fetching music:", error);
+        }
+    }, []);
+
+    return {
+        isPlaying,
+        musicData,
+        audioRef,
+        handlePlayPause,
+        handleNextMusic,
+    };
 };
